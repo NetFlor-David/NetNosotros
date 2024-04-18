45bb4e3c051416d22a762235c062bee61a54e0bc
Florsi <104874720+fflorsi@users.noreply.github.com>
2024-04-18 12:31:14 -0300

    Unidad02

---
+++ Unidad02/Capitulo01/Lab01/Decision/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputTexto;
+            Console.Write("Ingrese un texto"+'\n');
+            inputTexto = Console.ReadLine();
+            if (inputTexto.Length > 0)
+            {
+                Console.WriteLine("El texto ingresado es: " + inputTexto);
+                Console.WriteLine("Menu " +'\n' +
+                    "1- Mostrar la frase ingresada en Mayusculas" + '\n' +
+                    "2- Mostrar la frase ingresada en Minusculas" + '\n' +
+                    "3- Mostrar la cantidad de carácteres en la frase");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+               switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Su frase en mayúsculas: "+ '\n'+ inputTexto.ToUpper());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Su frase en mínusculas: " + '\n' +inputTexto.ToLower());
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("La longitud de su frase es: " + '\n' + inputTexto.Length);
+                        break;
+                    default:
+                        Console.WriteLine("La opción seleccionada no es correcta");
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No se ingreso texto");
+            }
+
+
+
+        }
+    }
+}
---
+++ Unidad02/Capitulo01/Lab01/Iteracion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] arreglo = new string[cantIteraciones];
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                string inputTexto;
+                Console.Write("Ingrese un texto" + '\n');
+                inputTexto = Console.ReadLine();
+                arreglo[i] = inputTexto;   
+            }
+            for (int i = cantIteraciones-1; i >= 0; i--)
+            {
+                Console.WriteLine("Texto ingresado en la posición " + (i+1));
+                Console.WriteLine(arreglo[i]);
+            }
+        }
+    }
+}
